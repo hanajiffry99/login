d19fb119c7071cbddf89e3e2f3e974ac867eb20c
Hana Jiffry <hanajiffryscg@gmail.com>
2021-03-25 23:12:14 +0530

    styling the form

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,22 +3,24 @@ import React from 'react';
 class Form extends React.Component {
     render() {
         return (
-            <div className="container">
+            <div>
                 <form>
                     <div class="mb-3">
-                        <label for="exampleInputEmail1" class="form-label">Email address</label>
-                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                        <label for="exampleInputEmail1" class="form-label">Username</label>
+                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" required/>
                             <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div> 
                     </div>
+                    <br/>
                         <div class="mb-3">
                             <label for="exampleInputPassword1" class="form-label">Password</label>
-                            <input type="password" class="form-control" id="exampleInputPassword1"/>
+                            <input type="password" class="form-control" id="exampleInputPassword1" required/>
                     </div>
                             <div class="mb-3 form-check">
                                 <input type="checkbox" class="form-check-input" id="exampleCheck1"/>
                                     <label class="form-check-label" for="exampleCheck1">Check me out</label>
                     </div>
-                                <button type="submit" class="btn btn-primary">Submit</button>
+                    <br/>
+                                <button type="submit" className="btn btn-primary">Submit</button>
                 </form>
            </div>
         );
